f8f3b0119ad15343d34978800a2e35b6f2b2d991
rohanagarwal <rohan.agarwal204@gmail.com>
2023-04-07 21:20:22 -0400

    KnowledgeTest done

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,36 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
+    address public owner;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
         string[] memory t = tokens;
         t[0] = "VET";
+        tokens = t;
+    }
+
+    receive() external payable {
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address addr) external {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool success, bytes memory data) = addr.call{value: getBalance()}("");
+        require(success);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata a, string calldata b) public pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
     }
 }
